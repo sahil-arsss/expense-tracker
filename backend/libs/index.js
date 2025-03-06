326be1e5eacba89189ce27e8fc427167d9f8fc0c
sahil-arsss <sahilchaudhary8876@gmail.com>
2025-03-07 02:13:40 +0530

    transaction route added

--- backend/libs/index.js
+++ backend/libs/index.js
@@ -23,3 +23,21 @@ export const createJWT= (id)=> {
     return JWT.sign({userId: id},process.env.JWT_SECRET,{expiresIn:"1d"})
     
 }
+
+export function getMonthName(index) {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return months[index];
+  }
