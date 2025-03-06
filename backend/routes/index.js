b4b3a33f79e03e63e77d66bed003105e3ede2225
sahil-arsss <sahilchaudhary8876@gmail.com>
2025-03-06 22:01:38 +0530

    account-controller with its route done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import authRoutes from './authroutes.js'
 import userRoutes from './userroutes.js'
+import accountroutes from './accountroutes.js'
 const routes=express.Router();
-
 
 routes.use('/auth',authRoutes);
 routes.use('/user',userRoutes);
+routes.use('/account',accountroutes);
 export default routes;
